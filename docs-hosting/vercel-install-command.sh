04d4b8f8a6c1ff8c89db492b73336cf3dcb03333
takagiy <takagiy.4dev@gmail.com>
2023-10-30 15:09:58 +0900

    update

---
+++ docs-hosting/vercel-install-command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+export RUSTUP_HOME=/vercel/.rustup
+export CARGO_HOME=/vercel/.cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+export PATH="$CARGO_HOME/bin:$PATH"
+npm install
